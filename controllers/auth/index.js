ec675416e6f8d7d45663c4c6123705832a4894e0
pttkachuk <petrotkachuk6@gmail.com>
2024-02-05 14:07:59 +0100

    create index.js in controllers folder

---
+++ controllers/auth/index.js
@@ -0,0 +1,11 @@
+const getCurrent = require('./getCurrent');
+const register = require('./register');
+const login = require('./login');
+const logout = require('./logout');
+
+module.exports = {
+    getCurrent,
+    register,
+    login,
+    logout,
+};
