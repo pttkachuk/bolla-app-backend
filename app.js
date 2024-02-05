4158a3e37612da985740dbb787e0afd5835f1f4b
pttkachuk <petrotkachuk6@gmail.com>
2024-02-05 11:40:49 +0100

    upd app js

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv').config();
-//console.log(process.env);
+const express = require('express')
+const logger = require('morgan')
+const cors = require('cors')
 
-const { DB_HOST } = process.env;
+const app = express()
 
-mongoose.set('strictQuery', false);
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
-mongoose.connect(DB_HOST)
-    .then(() => console.log('database connected'))
-    .catch((error) => console.log(error.message))
+app.use(logger(formatsLogger))
+app.use(cors())
+app.use(express.json())
+
+
+
+app.use((req, res) => {
+    res.status(404).json({ message: 'Not found' })
+})
+
+app.use((err, req, res, next) => {
+    const { status = 500, message = "Server error" } = err;
+    res.status(status).json({ message, })
+})
+
+module.exports = app
