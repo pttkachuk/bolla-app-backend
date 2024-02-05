844897acc3fcebbbd4ae32a474766d42f5b55af0
pttkachuk <petrotkachuk6@gmail.com>
2024-02-05 14:08:14 +0100

    upd app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
+require('dotenv').config()
+
+const authRouter = require('./routes/api/auth');
 
 const app = express()
 
@@ -10,7 +13,7 @@ app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
 
-
+app.use('/api/auth', authRouter)
 
 app.use((req, res) => {
     res.status(404).json({ message: 'Not found' })
