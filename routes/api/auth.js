443d363e2b4af21e5709098b9127a9bdb2760f38
pttkachuk <petrotkachuk6@gmail.com>
2024-02-05 14:06:50 +0100

    create routes.auth

---
+++ routes/api/auth.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const validateBody = require('../../middlewares/validateBody');
+const authenticate = require('../../middlewares/authenticate');
+const ctrlWrapper = require('../../helpers/ctrlWrapper');
+const { schemas } = require('../../schemas/authSchemas');
+const router = express.Router();
+const ctrl = require('../../controllers/auth');
+
+//signup
+router.post('/register', validateBody(schemas.registerSchema), ctrlWrapper(ctrl.register));
+//signin
+router.post('/login', validateBody(schemas.loginSchema), ctrlWrapper(ctrl.login));
+//signout
+router.get('/logout', authenticate, ctrlWrapper(ctrl.logout));
+//getcurrent
+router.get('/current', authenticate, ctrlWrapper(ctrl.getCurrent));
+
+module.exports = router;
