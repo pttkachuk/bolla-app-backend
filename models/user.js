de2e31ff784c9f6523da1bfa3001f54bf2d4dd6f
pttkachuk <petrotkachuk6@gmail.com>
2024-02-07 16:43:20 +0100

    add errMsg to user model

--- models/user.js
+++ models/user.js
@@ -1,23 +1,26 @@
 const { Schema, model } = require('mongoose');
 const emailRegexp = require('../helpers/emailRegexp');
 const handleSaveErrors = require('../helpers/handleSaveErrors');
+const errMsg = require('../helpers/errors');
 
 const userSchema = new Schema(
     {
         name: {
             type: String,
-            required: true,
+            minlenght: [3, errMsg.errFieldMinLength('User name', 3)],
+            maxlenght: [35, errMsg.errFieldMaxLength('User name', 35)],
+            required: [true, errMsg.errFieldIsRequired('User name')],
         },
         password: {
             type: String,
-            minlenght: 6,
-            required: [true, 'Set password for user'],
+            minlenght: [6, errMsg.errMsgMinPass],
+            required: [true, errMsg.errFieldIsRequired('Password')],
         },
         email: {
             type: String,
-            match: emailRegexp,
-            required: [true, 'Email is required'],
             unique: true,
+            match: emailRegexp,
+            required: [true, errMsg.errFieldIsRequired('Email')],
         },
         token: {
             type: String,
