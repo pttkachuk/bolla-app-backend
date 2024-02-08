2255525b313ffa810b94a63ed0c4f86b15163044
pttkachuk <petrotkachuk6@gmail.com>
2024-02-08 09:43:47 +0100

    upd register controller

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -3,16 +3,16 @@ const { User } = require('../../models/user');
 const bcrypt = require('bcryptjs');
 
 const register = async (req, res) => {
-    const { name, email, password } = req.body;
-    const user = await User.findOne({ email });
+    const { name, login, password } = req.body;
+    const user = await User.findOne({ login });
     if (user) {
         throw RequestError(409, 'Email in use');
     };
     const hashPassword = await bcrypt.hash(password, 10);
-    const result = await User.create({ name, email, password: hashPassword });
+    const result = await User.create({ name, login, password: hashPassword });
     res.status(201).json({
         name: result.name,
-        email: result.email
+        login: result.login
     });
 };
 
