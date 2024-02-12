c504566c638af71a3813cff795bde11dbd0f9fe6
pttkachuk <petrotkachuk6@gmail.com>
2024-02-12 11:03:20 +0100

    add resetPassword to index.js

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -2,10 +2,12 @@ const getCurrent = require('./getCurrent');
 const register = require('./register');
 const login = require('./login');
 const logout = require('./logout');
+const resetPassword = require('./resetPassword');
 
 module.exports = {
     getCurrent,
     register,
     login,
     logout,
+    resetPassword,
 };
